968a0c4cc6e8f2a15891825063848d89b253ac7a
elfi <gugl6369@gmail.com>
2025-10-19 23:45:01 +0000

    feat(script.js): start js code

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
-const formBlock = document.getElementById("form-block");
-const includeBlock = document.getElementById("include-block");
+const getInput = document.getElementById("input"); // input
+const getBtn = document.getElementById("form_button"); // button click
 
+const includeBlock = document.getElementById("include-block"); //block add
+
+getBtn.addEventListener("click", (event)=> {
+    event.preventDefault();
+    const file = getInput.files[0];
+    const fr = new FileReader();//кто ты, чмо.......
+
+    
+})
+
+// заметки мои
+
+// парсить ток стринги через file reader - 
+
+// - при загрузке файла появляется кнопка*(идея)
