2a684f2c5213f07ab6f1e06a115f9ac41b6807d6
elfi <gugl6369@gmail.com>
2025-10-20 11:03:54 +0000

    feat(script.js): new functions: -readerJSON, -closeBlock

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,38 @@
 const getInput = document.getElementById("input"); // input
 const getBtn = document.getElementById("form_button"); // button click
+const formBlock = document.getElementById("form-block");
 
-const includeBlock = document.getElementById("include-block"); //block add
+const includeBlock = document.getElementById("include"); //block add
+
+
 
 getBtn.addEventListener("click", (event)=> {
     event.preventDefault();
-    const file = getInput.files[0];
-    const fr = new FileReader();//кто ты, чмо.......
 
-    
-})
+    const file = getInput.files[0];
+    closeBlock();
+    readerJSON(file, (fileParse) =>{
+
+            fileParse.fields.forEach(field => {
+                const label = document.createElement('label'); 
+                label.textContent = field.label;
+                includeBlock.appendChild(label);
+            });
+    })
+});
+
+function closeBlock(){
+    formBlock.style.display = 'none';
+}
+
+function readerJSON(file, back){
+    const fr = new FileReader();
+    fr.readAsText(file);
+    fr.onload = () =>{
+        const fileParse = JSON.parse(fr.result);
+        back(fileParse);
+    }
+}
 
 // заметки мои
 
