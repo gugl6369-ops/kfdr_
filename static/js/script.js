ff744b822af7338060a35b892223fafb6c24fa14
elfi <gugl6369@gmail.com>
2025-10-21 00:57:27 +0000

    feat(css): +styles

--- static/js/script.js
+++ static/js/script.js
@@ -2,29 +2,34 @@ const getInput = document.getElementById("input"); // input
 const getBtn = document.getElementById("form_button"); // button click
 const formBlock = document.getElementById("form-block");
 
+const mainBlock = document.getElementById("main");
 const includeBlock = document.getElementById("include"); //block add
 let i = 1;
 
 
 getBtn.addEventListener("click", (event)=> {
     event.preventDefault();
-
+    mainBlock.style.display = 'block';
     const file = getInput.files[0];
     closeBlock();
     readerJSON(file, (fileParse) =>{
+            
             //name create
             const input_name = document.createElement('h1');
             input_name.textContent = fileParse.name;
             input_name.classList.add('include_name');
+            
             includeBlock.appendChild(input_name);
 
-
+            const field_block = document.createElement('div');
+            field_block.classList.add('include_field-block');
+            includeBlock.appendChild(field_block);
             fileParse.fields.forEach(field => {
-                try{
+                
                 //block create
                 const input_block = document.createElement('div');
-                input_block.classList.add('input-block');
-                includeBlock.appendChild(input_block);
+                input_block.classList.add('include_input-block');
+                field_block.appendChild(input_block);
 
                 //label create
                 if (field.label) label = createLabel(field.label, input_block);
@@ -37,21 +42,22 @@ getBtn.addEventListener("click", (event)=> {
                     label.htmlFor = input.id;
                     i++;
                 }
-                }
-                catch{
-                    alert('Error!')
-                }
+               
                 
             });
+            const btn_block = document.createElement('div');
+            btn_block.classList.add('include_btn-block');
+            includeBlock.appendChild(btn_block);
+
             fileParse.buttons.forEach(button => {
                 const include_btn = document.createElement('button');
                 include_btn.textContent = button.text;
-                include_btn.classList.add('include_btn');
-                includeBlock.appendChild(include_btn);
+                include_btn.classList.add('btn', 'include_btn');
+                btn_block.appendChild(include_btn);
             })
-        
 
     })
+    
 });
 
 
@@ -59,7 +65,7 @@ getBtn.addEventListener("click", (event)=> {
 function createLabel(name, path){
     const block = document.createElement('label');
     block.textContent = name;
-    block.classList.add('include_name');
+    block.classList.add('include_label');
     path.appendChild(block);
     return block;
 }
@@ -70,11 +76,13 @@ function createInput(name, path){
     const fieldInput = name;
     input.type = fieldInput.type;
     input.id = i;
-
+    input.classList.add('include_input');
     if (fieldInput.placeholder) input.setAttribute('placeholder', fieldInput.placeholder);
     if (fieldInput.required) input.required = true;
     if (fieldInput.multiple) input.multiple = true;
-    if (fieldInput.filetype) input.setAttribute('accept', fieldInput.filetype);
+    if (fieldInput.filetype) {
+
+    }
     path.appendChild(input);
     return input;
 }
@@ -85,11 +93,16 @@ function closeBlock(){
 }
 
 function readerJSON(file, back){
-    const fr = new FileReader();
-    fr.readAsText(file);
-    fr.onload = () =>{
-        const fileParse = JSON.parse(fr.result);
-        back(fileParse);
+    try{
+        const fr = new FileReader();
+        fr.readAsText(file);
+        fr.onload = () =>{
+            const fileParse = JSON.parse(fr.result);
+            back(fileParse);
+        }
+    }
+    catch{
+        alert('Error!');
     }
 }
 
@@ -98,3 +111,4 @@ function readerJSON(file, back){
 // парсить ток стринги через file reader - 
 
 // - при загрузке файла появляется кнопка*(идея)
+// крест для закрытия формы обратно
