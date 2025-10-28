7249f3281f9d311a41f82a1e04b94bcaf61fc6db
elfi <gugl6369@gmail.com>
2025-10-28 12:36:19 +0000

    style(js): add EventListener  active lang

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const cardList = document.querySelector(".our-work_card-list");
-const cards = document.querySelectorAll(".our-work_card");
-
-
+const cardList = document.querySelector(".our-work_card-list"),
+      cards = document.querySelectorAll(".our-work_card"),
+      separator = document.querySelectorAll(".header_separator");
+let active = separator[0];
 
 cards.forEach( card => {
     const btns = card.querySelectorAll(".our-work_scroll-title");
@@ -13,3 +13,37 @@ cards.forEach( card => {
     })
 })
 
+
+separator.forEach( (lang) => {
+    
+    lang.addEventListener('click', () =>{
+        if (lang != active) {
+            active = lang;
+            activeLang(lang);
+            if(active == separator[1]) {
+                deActiveLang(separator[0]);
+            }
+            else{
+                deActiveLang(separator[1]);
+            }
+        }
+        else{
+            lang.classList.remove('header_separator--active');
+            if(lang == separator[1]) {
+                active = separator[0];
+                activeLang(separator[0]);
+            }
+            else{
+                active = separator[1];
+                activeLang(separator[1]);
+            }
+        }
+    })
+})
+
+function activeLang(a){
+    a.classList.add('header_separator--active');
+}
+function deActiveLang(a){
+    a.classList.remove('header_separator--active');
+}
