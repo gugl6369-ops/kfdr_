ab068d0b0c14b3c8eff6594424d23d115c5a607f
elfi <gugl6369@gmail.com>
2025-11-09 22:44:12 +0000

    feat(vue, ts): api connect,

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,19 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
 
-  return { count, doubleCount, increment }
+export const useCounterStore = defineStore('counter', () => {
+  const id = ref([]);  
+  const getNews = () =>{
+    fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
+      .then((d)=>{
+        return d.json();
+      })
+      .then((f)=>{
+        id.value = f;
+      });
+  }
+  
+
+  return {getNews, id};
 })
---
+++ src/views/lol.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import {useCounterStore} from '../stores/counter';
+    
+    const store = useCounterStore();
+    store.getNews();
+</script>
+<template>
+    Приветщвыомптфывшиотв шцушгпрышвфгп рывшгп рфывлп
+    <p v-for="id in store.id">{{id}}</p>
+</template>
+<style>
+</style>
